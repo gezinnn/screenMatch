3470d19e73c2f18053294e92489998b177c28b88
Geovannni Almeida <131813766+gezinnn@users.noreply.github.com>
2025-09-09 16:04:38 -0300

    refactor: delegar execução do menu para a classe Main

--- src/main/java/br/com/alura/sreenchmatch/SreenchmatchApplication.java
+++ src/main/java/br/com/alura/sreenchmatch/SreenchmatchApplication.java
@@ -1,8 +1,6 @@
 package br.com.alura.sreenchmatch;
 
-import br.com.alura.sreenchmatch.model.DadosSerie;
-import br.com.alura.sreenchmatch.service.ConsumoAPI;
-import br.com.alura.sreenchmatch.service.ConverteDados;
+import br.com.alura.sreenchmatch.main.Main;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,19 +8,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SreenchmatchApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SreenchmatchApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SreenchmatchApplication.class, args);
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
-		var consumoAPI = new ConsumoAPI();
-		var json = consumoAPI.obterDados("https://www.omdbapi.com/?t=arrow&apiKey=af9672c7");
-		System.out.println(json);
+    @Override
+    public void run(String... args) throws Exception {
 
-		ConverteDados converteDados = new ConverteDados();
-		DadosSerie dadosSerie = converteDados.obterDados(json, DadosSerie.class);
-		System.out.println(dadosSerie);
+        Main principal = new Main();
+        principal.exibeMenu();
 
-	}
+
+    }
 }
